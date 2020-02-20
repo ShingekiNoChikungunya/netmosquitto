347fd249a5f1a5bc256555305fa87c2aea4048bc
RafaBonin <fael.bp_04@hotmail.com>
2020-02-20 08:58:09 -0300

    Added variables for buffer size and timout time; added some prints to show data sent and received as well as some comments for the code

--- netmosquito.py
+++ netmosquito.py
@@ -1,5 +1,8 @@
 import socket, sys
 
+buffer_size = 1				#How much data would you like to receive at once (bytes)
+timeout_time= 2				#Timeout to not keep holding forever in case of incomplete packet
+
 if len(sys.argv)==3:
 	host=sys.argv[1] 		#Host IP goes here
 	port=int(sys.argv[2]) 	#Port Goes Here
@@ -10,30 +13,30 @@ else:
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-s.settimeout(5)				#Sets the timeout time
+print(f'Connection established with {host}:{str(port)}\n')
+s.settimeout(timeout_time)	#Sets the timeout time
 
 def receive_data(s):
-	#This functions recieves data in 4 bytes chunks until '\n' or '\r' is received
+	#This functions recieves data in n bytes chunks until '\n' is received or timeout ocurrs
 	data=''
-	try:
-		#Prevents exception from timeouts
-		data+=s.recv(1).decode()
+	try:					#Prevents exception from timeouts
+		data+=s.recv(buffer_size).decode()
 	except socket.timeout:
 		return data
 
 	while data[-1]!='\n':
-		try:
-			#Prevents exception from timeouts
-			data+=s.recv(1).decode()
+		try:					#Prevents exception from timeouts
+			data+=s.recv(buffer_size).decode()
 		except socket.timeout:
 			break
 
-	print(data,end='')
+	if data != '\n':
+		print(f'Received data:\n{data}')
 	return data
 
 def parse_data(raw_data):
-	parsed_data=None
 	#This function parses the data received
+	parsed_data=None
 	'''
 		The parsing Logic goes here, make shure to return parsed data
 	'''
@@ -42,10 +45,15 @@ def parse_data(raw_data):
 def send_data(s,response):
 	#This function sends the response to the server
 	if response:
-		s.send(bytes(response,'utf-8'))
+		print(f'Sending data:\n{response}')
+		s.send(bytes(response+'\n','utf-8'))
 
 while True:
 	raw_data = receive_data(s)
+	if not raw_data:
+		print("\nConnection closed by server, exiting...")
+		s.close()
+		break
 	parsed_data = parse_data(raw_data)
 	'''
 		The logic of the Challange after the parsing goes here
@@ -53,8 +61,8 @@ while True:
 	try:
 		send_data(s,parsed_data)
 	except ConnectionResetError:
-		print("Connection closed by server")
+		print("\nConnection closed by server, exiting...")
 		break
 	except Exception as error:
-		print(f"An error has occured '{error}'")
+		print(f"\nAn error has occured '{error}'")
 		break
