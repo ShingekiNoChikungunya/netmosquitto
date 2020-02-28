6376ed1e8c64d7c15e6773667655bcdd0d3c4057
RafaBonin <fael.bp_04@hotmail.com>
2020-02-28 13:10:23 -0300

    Divided the netmosquito.py file into netmosquito.py and netmosquito_utils; applied pep8; still needs to be bug-tested

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # netmosquitto
+Basic 'netcat' for ppc challanges
+
+## Requirements:
+- python3
+
+## Usage:
+`python netmosquito.py <host> <port>`
--- netmosquito.py
+++ netmosquito.py
@@ -1,68 +1,48 @@
-import socket, sys
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-buffer_size = 1				#How much data would you like to receive at once (bytes)
-timeout_time= 2				#Timeout to not keep holding forever in case of incomplete packet
+from netmosquito_utils import *
 
-if len(sys.argv)==3:
-	host=sys.argv[1] 		#Host IP goes here
-	port=int(sys.argv[2]) 	#Port Goes Here
+buffer_size = 1     # How much data would you like to receive at once (bytes)
+timeout_time = 2    # Timeout to avoid blocking
 
-else:
-	print(('Error while running, please execute proggram as following:\npython netmosquito.py <host> <port>'))
-	exit()
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host,port))
-print(f'Connection established with {host}:{str(port)}\n')
-s.settimeout(timeout_time)	#Sets the timeout time
-
-def receive_data(s):
-	#This functions recieves data in n bytes chunks until '\n' is received or timeout ocurrs
-	data=''
-	try:					#Prevents exception from timeouts
-		data+=s.recv(buffer_size).decode()
-	except socket.timeout:
-		return data
-
-	while data[-1]!='\n':
-		try:					#Prevents exception from timeouts
-			data+=s.recv(buffer_size).decode()
-		except socket.timeout:
-			break
-
-	if data != '\n':
-		print(f'Received data:\n{data}')
-	return data
 
 def parse_data(raw_data):
-	#This function parses the data received
-	parsed_data=None
-	'''
+    '''Parses data. This function must be eddited by the user'''
+
+    parsed_data = None
+    '''
 		The parsing Logic goes here, make shure to return parsed data
 	'''
-	return parsed_data
+    return parsed_data
 
-def send_data(s,response):
-	#This function sends the response to the server
-	if response:
-		print(f'Sending data:\n{response}')
-		s.send(bytes(response+'\n','utf-8'))
+
+host, port = check_initial_params()
+s = socket_setup(host, port, timeout_time)
 
 while True:
-	raw_data = receive_data(s)
-	if not raw_data:
-		print("\nConnection closed by server, exiting...")
-		s.close()
-		break
-	parsed_data = parse_data(raw_data)
-	'''
+    raw_data = receive_data(s, buffer_size)
+
+    if not raw_data:
+        # If no data is received, that means the connection has been closed
+        # If you still want to keep receiving data, exclude this if (caution,
+        # this may cause an infinite loop)
+        print("\nConnection closed by server, exiting...")
+        s.close()
+        break
+    parsed_data = parse_data(raw_data)
+
+    '''
 		The logic of the Challange after the parsing goes here
 	'''
-	try:
-		send_data(s,parsed_data)
-	except ConnectionResetError:
-		print("\nConnection closed by server, exiting...")
-		break
-	except Exception as error:
-		print(f"\nAn error has occured '{error}'")
-		break
+
+    try:
+        send_data(s, parsed_data)
+    except ConnectionResetError:
+        print("\nConnection closed by server, exiting...")
+        s.close()
+        break
+    except Exception as error:
+        print(f"\nAn error has occured '{error}'")
+        s.close()
+        break
